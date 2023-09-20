8a069492b2b6a7781569fae0de453e5c44351970
Doglas Oliveira <doglas.oliveira@hotmail.com>
2023-09-20 12:20:04 +0100

    [FEAT]Cleaning code

--- controller/View1.controller.js
+++ controller/View1.controller.js
@@ -15,8 +15,6 @@ sap.ui.define([
 
             onSelecFile: function (event) {
                 const file = event.getParameters().files[0];
-
-                // Verifique se o arquivo é uma imagem
                 if (file.type.startsWith('image/')) {
                     const img = new Image();
                     const canvas = document.createElement('canvas');
@@ -25,49 +23,34 @@ sap.ui.define([
                     img.onload = () => {
                         let width = img.width;
                         let height = img.height;
-
-                        // Detecte a orientação da imagem e aplique a correção, se necessário
                         if (width > height) {
-                            // A imagem está em modo paisagem, então gire-a para o modo retrato
                             canvas.width = height;
                             canvas.height = width;
-                            ctx.rotate(Math.PI / 2); // Rotação de 90 graus (pi/2 radianos)
+                            ctx.rotate(Math.PI / 2);
                             ctx.drawImage(img, 0, -height, width, height);
                         } else {
-                            // A imagem já está em modo retrato, não é necessário girar
                             canvas.width = width;
                             canvas.height = height;
                             ctx.drawImage(img, 0, 0, width, height);
                         }
 
-                        // Converta o canvas para um Blob
                         canvas.toBlob((blob) => {
                             const reader = new FileReader();
                             reader.onloadend = () => {
                                 const dataURL = reader.result;
-
-                                // Criar um novo documento PDF usando jsPDF
                                 const doc = new jsPDF.jsPDF();
                                 const pdfWidth = doc.internal.pageSize.getWidth();
                                 const pdfHeight = doc.internal.pageSize.getHeight();
-
-                                // Adicione a imagem corrigida ao PDF usando o Data URL
                                 doc.addImage(dataURL, 'JPEG', 0, 0, pdfWidth, pdfHeight);
-
-                                // Salvar ou exibir o PDF, por exemplo:
-                                //doc.save('documento.pdf'); // Para salvar o PDF
-                                // Ou
-                                window.open(doc.output('bloburl'), '_blank'); // Para exibir o PDF em uma nova janela do navegador
+                                //doc.save('documento.pdf'); // Download pdf just test
+                                window.open(doc.output('bloburl'), '_blank'); 
                             };
 
                             reader.readAsDataURL(blob);
                         }, file.type);
                     };
-
-                    // Carregue a imagem
                     img.src = URL.createObjectURL(file);
                 }
-
             }
         });
     });
